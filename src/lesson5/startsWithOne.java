117ee46b730e2f3ced5d281564f6c9f2990e7bf1
vishal <vishal.vishwakarma20@gmail.com>
2025-05-06 23:06:47 +0530

    new program added

---
+++ src/lesson5/startsWithOne.java
@@ -0,0 +1,59 @@
+package lesson5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class startsWithOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//  Find the number starts with 1
+/*	int [] number = {67, 1, 12, 23, 21, 10};
+	List<Integer> startsWithOne =  getNumberList(number);
+	System.out.println(startsWithOne);
+	
+	}
+
+	public static int getFirstDigit(int num)   {
+	while (num>=10)   {
+	num=num/10;	
+	}
+	return num;
+	}
+
+	public static ArrayList<Integer> getNumberList(int [] number)   {
+	ArrayList<Integer> arl = new ArrayList<Integer>();
+	for (int num:number )   {
+	int n = getFirstDigit(num);
+	if (n==1)   {
+	arl.add(num);
+	}
+	}
+	return arl;   */
+		
+//  For Practice
+	int [] number = {67, 1, 12, 23, 21, 10};
+	List<Integer> startsWithOne = getNumberList(number);
+	System.out.println(startsWithOne);
+	
+	}
+	
+	public static int getFirstDigit(int num)   {
+	while (num>=10)   {
+	num=num/10;	
+	}
+	return num;
+	}
+
+	public static ArrayList<Integer> getNumberList(int [] number)   {
+    ArrayList<Integer> arl = new ArrayList<Integer>();
+	for (int num:number)   {
+	int n = getFirstDigit(num);
+	if (n==1)   {
+	arl.add(num);	
+	}
+	}
+	return arl;
+	}
+
+}
