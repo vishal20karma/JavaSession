a15ae3b0dd1c53212493ddd19866531698ae39e0
vishal <vishal.vishwakarma20@gmail.com>
2025-05-28 10:35:37 +0530

    new changes

--- src/lesson1/findVowels.java
+++ src/lesson1/findVowels.java
@@ -6,7 +6,7 @@ public class findVowels {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//  How to find the vowels in a string with user input (scanner)
 /*	Scanner s = new Scanner(System.in);
 //  Input from user
     System.out.print("Enter a string: ");
@@ -14,7 +14,7 @@ public class findVowels {
 //  Define vowels
     String vowels = "AEIOUaeiou";
     System.out.println("Vowels in the String are: ");
-    for (int i = 0; i < input.length(); i++) {
+    for (int i = 0; i < input.length(); i++) {   //remove '!' and put double '==', this program will return 'non vowels'
     char c = input.charAt(i);
     if (vowels.indexOf(c)!=-1) {
     System.out.print(c+" ");
@@ -30,7 +30,7 @@ public class findVowels {
 	System.out.println("Vowels in the String are: ");
 	for (int i=0;i<input.length();i++)   {
 	char c = input.charAt(i);	
-	if (vowels.indexOf(c)!=-1)   {
+	if (vowels.indexOf(c)!=-1)   {   //remove '!' and put double '==', this program will return 'non vowels'
 	System.out.print(c+" ");	
 	}
 	}
